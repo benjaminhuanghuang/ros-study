b061bd6f8adbff01a3a6c7742270da41c5b7e8e2
Benjamin Huang <benjhuang@microsoft.com>
2022-09-25 15:39:14 -0700

    urdf exmaple

--- _Courses/self-driving-vehicle-101/catkin_ws/src/box/launch/spawn_box.launch.py
+++ _Courses/self-driving-vehicle-101/catkin_ws/src/box/launch/spawn_box.launch.py
@@ -1,98 +1,34 @@
 import os
 from launch import LaunchDescription
+from ament_index_python.packages import get_package_share_directory
 from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription
 from launch.conditions import IfCondition, UnlessCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import Command, LaunchConfiguration, PythonExpression
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
-
+import xacro
 
 def generate_launch_description():
-    # Constants for paths to different files and folders
-    gazebo_models_path = 'models'
-    package_name = 'box'
-    robot_name_in_model = 'box_robot'
-    urdf_file_path = 'urdf/box.urdf'
-
-    # Pose where we want to spawn the robot
-    spawn_x_val = '0.0'
-    spawn_y_val = '0.0'
-    spawn_z_val = '0.0'
-    spawn_yaw_val = '0.00'
-
-    # Set the path to different files and folders.  
-    pkg_gazebo_ros = FindPackageShare(package='gazebo_ros').find('gazebo_ros')   
-    pkg_share = FindPackageShare(package=package_name).find(package_name)
-    default_urdf_model_path = os.path.join(pkg_share, urdf_file_path)
-    gazebo_models_path = os.path.join(pkg_share, gazebo_models_path)
-    os.environ["GAZEBO_MODEL_PATH"] = gazebo_models_path
     
-    # Launch configuration variables specific to simulation
-    gui = LaunchConfiguration('gui')
-    headless = LaunchConfiguration('headless')
-    namespace = LaunchConfiguration('namespace')
-    urdf_model = LaunchConfiguration('urdf_model')
-    use_namespace = LaunchConfiguration('use_namespace')
-    use_robot_state_pub = LaunchConfiguration('use_robot_state_pub')
-    use_sim_time = LaunchConfiguration('use_sim_time')
-    use_simulator = LaunchConfiguration('use_simulator')
-    
-    declare_simulator_cmd = DeclareLaunchArgument(
-        name='headless',
-        default_value='False',
-        description='Whether to execute gzclient')
- 
-    declare_urdf_model_path_cmd = DeclareLaunchArgument(
-        name='urdf_model', 
-        default_value=default_urdf_model_path, 
-        description='Absolute path to robot urdf file')
-        
-    declare_use_robot_state_pub_cmd = DeclareLaunchArgument(
-        name='use_robot_state_pub',
-        default_value='True',
-        description='Whether to start the robot state publisher')
+    pkg_name = 'box'
+    file_subpath = 'urdf/box.urdf'
+    pkg_share = FindPackageShare(package=pkg_name).find(pkg_name) 
+    urdf_model_path = os.path.join(pkg_share, file_subpath)
 
-    declare_use_simulator_cmd = DeclareLaunchArgument(
-        name='use_simulator',
-        default_value='True',
-        description='Whether to start the simulator')
+    xacro_file = os.path.join(get_package_share_directory(pkg_name),file_subpath)
+    robot_description_raw = xacro.process_file(xacro_file).toxml()
 
-    # Start Gazebo server
-    start_gazebo_server_cmd = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(os.path.join(pkg_gazebo_ros, 'launch', 'gzserver.launch.py')),
-        condition=IfCondition(use_simulator))
- 
-    # Start Gazebo client    
-    start_gazebo_client_cmd = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(os.path.join(pkg_gazebo_ros, 'launch', 'gzclient.launch.py')),
-        condition=IfCondition(PythonExpression([use_simulator, ' and not ', headless])))
+    # Configure the node
+    node_robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[{'robot_description': robot_description_raw}]
+    )
 
-    # Launch the robot
-    spawn_entity_cmd = Node(
-        package='gazebo_ros', 
-        executable='spawn_entity.py',
-        arguments=['-entity', robot_name_in_model, 
-                    '-topic', 'robot_description',
-                        '-x', spawn_x_val,
-                        '-y', spawn_y_val,
-                        '-z', spawn_z_val,
-                        '-Y', spawn_yaw_val],
-                        output='screen')
 
-    # Create the launch description and populate
-    ld = LaunchDescription()
-    
-    # Declare the launch options
-    ld.add_action(declare_simulator_cmd)
-    ld.add_action(declare_urdf_model_path_cmd)
-    ld.add_action(declare_use_robot_state_pub_cmd)  
-    ld.add_action(declare_use_simulator_cmd)
-    
-    # Add any actions   
-
-    ld.add_action(start_gazebo_server_cmd)
-    ld.add_action(start_gazebo_client_cmd)
-    ld.add_action(spawn_entity_cmd)
-
-    return ld
+    # Run the node
+    return LaunchDescription([
+        node_robot_state_publisher
+    ])
